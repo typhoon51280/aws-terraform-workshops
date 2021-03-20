1012993ea133cefbb7cf677d9f84c2df567b1e05
vincenzo.delloste <vincenzo.delloste@nttdata.com>
2021-03-20 08:40:36 +0100

    w4

---
+++ w4/remote_state/creds.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+    profile = "test"
+    access_key = ""
+    secret_key = ""
+    region = "eu-west-1"
+}
--- w4/remote_state/s3.tf
+++ w4/remote_state/s3.tf
@@ -1,2 +1,14 @@
 # Create S3 bucket that will be used to store terraform remote state
 # Make sure that versioning is enabled for bucket
+
+resource "aws_s3_bucket" "tf-remote-state-bucket" {
+  bucket = var.s3_bucket_name
+  acl = "private"
+  versioning {
+    enabled = true
+  }
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.tf-remote-state-bucket.bucket
+}
