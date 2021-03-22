465f2ed4dd14fcea19fd6737a48788b9ccdc645a
vincenzo.delloste <vincenzo.delloste@nttdata.com>
2021-03-22 10:16:27 +0100

    fix installation jenkins plugin

--- w4/jenkins/files/jenkins_bootstrap_script.sh
+++ w4/jenkins/files/jenkins_bootstrap_script.sh
@@ -15,7 +15,7 @@ sed "s/^JENKINS_JAVA_OPTIONS=.*/JENKINS_JAVA_OPTIONS=\"-Djava.awt.headless=true 
 sed "s/^JENKINS_PORT=.*/JENKINS_PORT=\"8000\"/g" -i /etc/sysconfig/jenkins
 
 wget -O /var/lib/jenkins/jenkins-plugins-manager.jar "https://github.com/jenkinsci/plugin-installation-manager-tool/releases/download/2.9.0/jenkins-plugin-manager-2.9.0.jar"
-sh -c 'java -jar /var/lib/jenkins/jenkins-plugins-manager.jar --war /usr/lib/jenkins/jenkins.war --verbose -d /var/lib/jenkins/plugins --plugins configuration-as-code:1.47 jenkins-plugin-cli job-dsl:1.77'
+sh -c 'java -jar /var/lib/jenkins/jenkins-plugins-manager.jar --war /usr/lib/jenkins/jenkins.war --verbose -d /var/lib/jenkins/plugins --plugins configuration-as-code job-dsl'
 
 chown -R jenkins.jenkins /var/lib/jenkins
 
