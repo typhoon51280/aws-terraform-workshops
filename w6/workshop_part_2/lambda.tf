306d444a3b6b98f0e5d50979ca9e731a808ff246
vincenzo.delloste <vincenzo.delloste@nttdata.com>
2021-03-24 07:04:42 +0100

    ec2instanceconnect

--- w6/workshop_part_2/lambda.tf
+++ w6/workshop_part_2/lambda.tf
@@ -1,21 +1,19 @@
-/*
-
 # Uncomment and add missing arguments here. Keep rest configuration as is.
 
 resource "aws_lambda_function" "ecs_autoscale_lambda" {
-  function_name = 
+  function_name = "w6-lambda"
 
   filename = "files/ecs_autoscale_lambda.zip"
-  role = "${aws_iam_role.w6-role.arn}"
-  runtime = "nodejs4.3"
+  role = aws_iam_role.w6-role.arn
+  runtime = "nodejs14.x"
   timeout = "8"
   handler = "lambda_ecs_autoscale.handler"
 
   environment {
     variables = {
-      asg_name = "${aws_autoscaling_group.w6-asg.name}"
-      ecs_cluster_name = "${aws_ecs_cluster.w6-ecs-cluster.name}"
-      ecs_service_name = "${aws_ecs_service.w6-ecs-service.name}"
+      asg_name = aws_autoscaling_group.w6-asg.name
+      ecs_cluster_name = aws_ecs_cluster.w6-ecs-cluster.name
+      ecs_service_name = aws_ecs_service.w6-ecs-service.name
     }
   }
 }
@@ -24,16 +22,15 @@ resource "aws_lambda_function" "ecs_autoscale_lambda" {
 resource "aws_lambda_permission" "ecs_autoscale_lambda_sns_permission" {
   statement_id = "AllowExecutionFromSNS"
   action = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.ecs_autoscale_lambda.arn}"
+  function_name = aws_lambda_function.ecs_autoscale_lambda.arn
   principal = "sns.amazonaws.com"
-  source_arn = "${aws_sns_topic.scale_notifications.arn}"
+  source_arn = aws_sns_topic.scale_notifications.arn
 }
 
 # AWS Lambda is subsribed to SNS topic so events that come to it will be processed by Lambda.
 resource "aws_sns_topic_subscription" "scale_sns_events" {
-  topic_arn = "${aws_sns_topic.scale_notifications.arn}"
+  topic_arn = aws_sns_topic.scale_notifications.arn
   protocol = "lambda"
-  endpoint  = "${aws_lambda_function.ecs_autoscale_lambda.arn}"
+  endpoint  = aws_lambda_function.ecs_autoscale_lambda.arn
 }
 
-*/
--- w6/workshop_part_2/sns.tf
+++ w6/workshop_part_2/sns.tf
@@ -1,18 +1,15 @@
-/*
-
 # Uncomment and add missing arguments here. Keep rest configuration as is.
 
 resource "aws_sns_topic" "scale_notifications" {
-  name =
+  name_name_prefix = "w6-"
 }
 
 resource "aws_autoscaling_notification" "ecs_asg_up_notifications" {
-  group_names = [ "${aws_autoscaling_group.w6-asg.name}" ]
+  group_names = [ aws_autoscaling_group.w6-asg.name ]
   notifications  = [
     "autoscaling:EC2_INSTANCE_LAUNCH",
     "autoscaling:EC2_INSTANCE_TERMINATE"
   ]
-  topic_arn = "${aws_sns_topic.scale_notifications.arn}"
+  topic_arn = aws_sns_topic.scale_notifications.arn
 }
 
-*/
